4e74b60dfcc50830dbfb347472668156d8db9eb1
Belginbalakrishanan <belginbalakrishnan@gmail.com>
2024-01-03 20:45:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "C:/Users/Belgin B/Desktop/student_data.txt";
+ 
+        if (File.Exists(filePath))
+        {
+            string[] lines = File.ReadAllLines(filePath);
+
+           
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        else
+        {
+            Console.WriteLine("File not found.");
+        }
+
+        Console.ReadLine(); 
+    }
+}
